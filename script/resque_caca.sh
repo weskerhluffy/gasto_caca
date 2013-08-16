a4b99394e9741d781deeeb9343c5f379da099515
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2013-08-15 22:58:14 -0500

    modificado script de lanzamiento de jobs
    se movio la inicializacion del servidor al principio de todo

--- script/resque_caca.sh
+++ script/resque_caca.sh
@@ -5,8 +5,8 @@
 # http://tommy.chheng.com/2010/10/26/how-to-run-background-processes-using-resqueredis-in-a-ruby-on-rails-app/
 # http://hafizbadrie.wordpress.com/2011/03/23/implementing-resqueredis-on-rails-3/
 # http://rubylearning.com/blog/2010/11/08/do-you-know-resque/
-rake resque:scheduler
+/Users/ernesto/programas/redis/src/redis-server &
 sleep 10
-COUNT=5 QUEUE=* rake resque:workers
-./src/redis-server
+rake resque:scheduler &
+COUNT=5 QUEUE=* rake resque:workers &
 echo "The IP to check this fucking shit http://0.0.0.0:5678/overview"
